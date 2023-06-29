90f6dfca8996c34620e9f786daf357899f8b02ca
yeshwenthgutta15 <131468733+yeshwenthgutta15@users.noreply.github.com>
2023-06-29 23:42:39 +0530

    Add files via upload

---
+++ prgram2.py
@@ -0,0 +1,31 @@
+#   Implement Merge Sort
+
+def merge_sort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list) // 2
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
+if __name__ == "__main__":
+    list = [7, 1, 9, 5, 6, 2, 8, 3, 0, 10, 4]
+    sorted_list = merge_sort(list)
+    print(sorted_list)
